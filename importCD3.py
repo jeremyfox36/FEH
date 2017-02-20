3d3b07c4b3dc884b5e3de538d7253337e6d7758c
@jeremyfox36 <@jeremyfox36@gmail.com>
2017-02-20 21:56:07 +0000

    Updated to put station details into database Started migrating to Django

--- importCD3.py
+++ importCD3.py
@@ -5,9 +5,27 @@ Created on Mon Jan  9 14:59:32 2017
 
 @author: jem
 """
+import os.path
+from os.path import basename
+import psycopg2
+import sqlite3
+import statistics
+import zipfile
+
+import lmoments
+import yaml
+from tkinter import *
+from tkinter import filedialog
+from tkinter.filedialog import askopenfilename
+import os
 
 import csv
-with open('/Users/jem/WINFAP-FEH_v4.1/Suitable for Pooling/3003.CD3') as f:
+root = Tk()
+root.withdraw()
+root.update()
+pathtoopen = filedialog.askopenfilename(initialdir="", title="choose CD3 file",
+                                           filetypes=(("CD3 files", "*.CD3"), ("all files", "*.*")))
+with open(pathtoopen) as f:
     d = dict(filter(None, csv.reader(f)))
 
 print(d)
