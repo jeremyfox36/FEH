963f73470b442b35eca7574d8019a8a5fef01ee0
@jeremyfox36 <@jeremyfox36@gmail.com>
2017-04-01 22:52:54 +0100

    SDM calculation moved to SQL query amax data returned form db to dictionary total years flow data implemented SDM.sdm_from_db function now returns a pooling group with at least 500 years of data

--- SDM.py
+++ SDM.py
@@ -1,66 +1,39 @@
-import math
-from pprint import pprint
-
-import numpy as np
-from collections import OrderedDict
 import psycopg2
 import psycopg2.extras
-from FEH import importcatchment
 
 
-# calculates the distance measure based on catchment descriptors.  i is imported ungauged catchment of interest,
-# j is gauged catchment from suitable for pooling file
-
-
-def sdm():
-
-    ungauged_catchment = importcatchment.import_catchment()
-
-    areai = ungauged_catchment.areai
-    saari = ungauged_catchment.saari
-    farli = ungauged_catchment.farli
-    fpexti = ungauged_catchment.fpexti
+def sdm_from_db():
+    total_years_data = 0  # keeps count of number of years data for formation of pooling group
+    pooling_group = {}
 
     conn = psycopg2.connect(dbname='feh1', user='jem', host='localhost', )
     dict_cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     dict_cur.execute(
-        'SELECT cd3_data.stationNum, cd3_data.dTMArea, cd3_data.saar, cd3_data.farl, cd3_data.fPExt FROM cd3_data '
-        'WHERE cd3_data.suitPooling=TRUE')
+        "SELECT catchment_name, cd3_data.stationnum,"
+        "sqrt((3.2::DOUBLE PRECISION * (((ln(user_ungauged_catchment.areai::DOUBLE PRECISION) - ln(cd3_data.dtmarea::DOUBLE PRECISION)) / 1.28::DOUBLE PRECISION) ^ 2::DOUBLE PRECISION))"
+        "+(0.5::DOUBLE PRECISION * (((ln(user_ungauged_catchment.saari::DOUBLE PRECISION) - ln(cd3_data.saar::DOUBLE PRECISION)) / 0.37::DOUBLE PRECISION) ^ 2::DOUBLE PRECISION))"
+        "+(0.1::DOUBLE PRECISION * (((user_ungauged_catchment.farli - cd3_data.farl) / 0.05::DOUBLE PRECISION) ^ 2::DOUBLE PRECISION))"
+        "+(0.2::DOUBLE PRECISION * (((user_ungauged_catchment.fpexti - cd3_data.fpext) / 0.04::DOUBLE PRECISION) ^ 2::DOUBLE PRECISION)))"
+        " AS SDM FROM user_ungauged_catchment, cd3_data WHERE user_ungauged_catchment.catchment_name::TEXT = '37017'::TEXT AND cd3_data.suitpooling = TRUE ORDER BY SDM")
 
     rows = dict_cur.fetchall()
-    pooling_group = {}
 
     for row in rows:
-
-        if (row['dtmarea'] or row['saar'] or row['farl'] or row['fpext']) is None:
-            row['sdm'] = None
-            pooling_group[row['stationnum']] = row
-        else:
-
-            areadiff = math.pow(((np.log(areai) - np.log(row['dtmarea'])) / 1.28), 2)
-
-            saardiff = math.pow(((np.log(saari) - np.log(row['saar'])) / 0.37), 2)
-
-            farldiff = math.pow((farli - row['farl']) / 0.05, 2)
-
-            fpextdiff = math.pow((fpexti - row['fpext']) / 0.04, 2)
-
-            row['sdm'] = math.sqrt((3.2 * areadiff) + (0.5 * saardiff) + (0.1 * farldiff) + (0.2 * fpextdiff))
-
-            nt_cur = conn.cursor()
+        if total_years_data <= 500:
+            rd_cur = conn.cursor()
             st = row['stationnum']
-            nt_cur.execute('SELECT flow FROM amaxdata WHERE stationnum=%s', (st,))
+            rd_cur.execute('SELECT flow FROM amaxdata WHERE stationnum=%s', (st,))
 
-            amaxdata = nt_cur.fetchall()
-            amaxdata = [flow[0] for flow in amaxdata]
+            amaxdata = rd_cur.fetchall()
+
+            amaxdata = [flow[0] for flow in amaxdata]  # changes list of tuples in tuple
             row['amaxdata'] = amaxdata
-
+            row['amaxcount'] = len(amaxdata)
             pooling_group[row['stationnum']] = row
-            all_stations_sdm = OrderedDict(pooling_group)
+            del(row['stationnum'])
+            total_years_data += len(amaxdata)
 
-    return all_stations_sdm
+        else:
+            break
 
-    # todo: make a new version  with select query changed to sdm_query and check speed of python vs SQL
-    # todo: add number of years data (count of amax list) to total
-    # todo: remove entry from dict
-    # todo: do until number of years = 500
+    return pooling_group
--- program.py
+++ program.py
@@ -2,39 +2,42 @@ from pprint import pprint
 
 from FEH.importcatchment import import_catchment
 from FEH.parse_zipfile import parse_zipfile
+from FEH import SDM
 
 
 def main():
-    parse_zipfile()
+    print(SDM.sdm_from_db())
+
     # gets the user selected ungauged catchment data in ready for SDM calculation
+
 
 # bring in CD3 file
 # bring in station data from database
 
-    # open existing project
-    # get project details from database for user
+# open existing project
+# get project details from database for user
 
-    # new project
-    # two tabs - Sites and Pooling group
+# new project
+# two tabs - Sites and Pooling group
 
-    # add a site to the sites tab and view the station data
-    # flask procedures for lists and file opening - SelectField, wtforms, request.form.getlist
+# add a site to the sites tab and view the station data
+# flask procedures for lists and file opening - SelectField, wtforms, request.form.getlist
 
-    # create a new station (ungauged site) from FEH CD-ROM data (.CD3 format)
+# create a new station (ungauged site) from FEH CD-ROM data (.CD3 format)
 
-    # begin estimation procedures
-    # single site analysis or pooled analysis?
+# begin estimation procedures
+# single site analysis or pooled analysis?
 
-    # create pooling group
-    # import ungauged catchment
-    # select all suitable for pooling stations from database and put into dictionary
-    # iterate over dictionary and calculate SDM for each station vs ungauged catchment
-    # sort stations by SDM
-    # move stations into new list (pooling_group) based on lowest SDM first until
-    # number of years data is at least 500
+# create pooling group
+# import ungauged catchment
+# select all suitable for pooling stations from database and put into dictionary
+# iterate over dictionary and calculate SDM for each station vs ungauged catchment
+# sort stations by SDM
+# move stations into new list (pooling_group) based on lowest SDM first until
+# number of years data is at least 500
 
-    # calculate growth curve
-    # use LMOM and enhanced procedure to do this
+# calculate growth curve
+# use LMOM and enhanced procedure to do this
 
 if __name__ == '__main__':
     main()
